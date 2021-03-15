d5963117900a2ed0641b498333e5dd9afaf4e9f6
amirarab888 <arabzadehamirhossein888@gmail.com>
2021-03-15 11:16:15 +0330

    Implement FileReader.cs

---
+++ Phase10/Phase10/Phase10Library/FileReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Phase10Library
+{
+    public class FileReader
+    {
+        public string GetFileContent(string filePath)
+        {
+            ValidateFilePath(filePath);
+            try
+            {
+                var content = File.ReadAllText(filePath);
+                return content;
+            }
+            catch (Exception fileNotFoundException)
+            {
+                Console.WriteLine(fileNotFoundException);
+                throw new FileNotFoundException(fileNotFoundException.Message);
+            }
+            
+        }
+
+        private void ValidateFilePath(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentException("Path is null or empty");
+            }
+        }
+    }
+}
---
+++ Phase10/Phase10/Phase10LibraryTest/FileReaderTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Moq;
+using Phase10Library;
+using Xunit;
+
+namespace InvertedIndexTest
+{
+    public class FileReaderTest
+    {
+        public static IEnumerable<object[]> GetFileContentInvalidArguments = new List<object[]>
+        {
+            new object[] {"chert"},
+            new object[] {"pert"},
+        };
+
+        [Theory]
+        [MemberData(nameof(GetFileContentInvalidArguments))]
+        public void GetFileContent_ShouldThrowFileNotFoundException_IfPathIsInvalid(string path)
+        {
+            var fileReader = new FileReader();
+            void Action() => fileReader.GetFileContent(path);
+            Assert.Throws<FileNotFoundException>(Action);
+        }
+        
+        
+        
+        
+        
+        
+        public static IEnumerable<object[]> GetFileContentNullOrEmptyArguments = new List<object[]>
+        {
+            new object[] {null},
+            new object[] {"    "},
+        };
+
+        [Theory]
+        [MemberData(nameof(GetFileContentNullOrEmptyArguments))]
+        public void GetFileContent_ShouldThrowArgumentException_IfPathIsNullOrEmpty(string path)
+        {
+            var fileReader = new FileReader();
+            void Action() => fileReader.GetFileContent(path);
+            Assert.Throws<ArgumentException>(Action);
+        }
+
+        [Fact]
+        public void GetFileContent_ShouldReturnWholeFileContent_WhenPathIsValid()
+        {
+            var fileReader = new FileReader();
+            Assert.Equal("ali, hasan hossein ....]][][}{}P{L{P\r\n",
+                fileReader.GetFileContent("GetContentTestFile.txt"));
+        }
+    }
+    
+}
