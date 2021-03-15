b09c675accef8706f6055658e3dfcc440d21bf34
amirarab888 <arabzadehamirhossein888@gmail.com>
2021-03-15 10:39:09 +0330

    Impl Doc entity

---
+++ Phase10/Phase10/Phase10Library/Doc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertedIndexLibrary
+{
+    public class Doc
+    {
+        public Doc(int id, string content)
+        {
+            ValidateId(id);
+            ValidateContent(content);
+            
+            Id = id;
+            Content = content;
+        }
+
+        private void ValidateContent(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        private void ValidateId(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public int Id { get; set; }
+        public string Content { get; set; }
+        
+    }
+}
---
+++ Phase10/Phase10/Phase10LibraryTest/DocTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using InvertedIndexLibrary;
+using Xunit;
+using Moq;
+
+
+namespace InvertedIndexTest
+{
+    public class DocTest
+    {
+        public static IEnumerable<object[]> GetDocArguments = new List<object[]>
+        {
+            new object[] {It.IsAny<int>(),"    "},
+            new object[] {It.IsAny<int>(),null},
+            new object[] {-1055,It.IsAny<string>()},
+        };
+        [Theory]
+        [MemberData(nameof(GetDocArguments))]
+        public void Doc_ShouldThrowArgumentException_WhenParametersAreInvalid(int id, string content)
+        {
+            void Action() => new Doc(id, content);
+            Assert.Throws<ArgumentException>(Action);
+        }
+    }
+}
