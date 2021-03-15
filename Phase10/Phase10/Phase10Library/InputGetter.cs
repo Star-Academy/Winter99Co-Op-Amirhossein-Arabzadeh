084b056c341934d7fdae665f2b7366240c66716c
amirarab888 <arabzadehamirhossein888@gmail.com>
2021-03-15 11:56:01 +0330

    Implement InputGetter.cs

---
+++ Phase10/Phase10/Phase10Library/InputGetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Phase10Library
+{
+    public interface IInputGetter
+    {
+        string GetInput();
+    }
+
+    public class InputGetter : IInputGetter
+    {
+        public string GetInput()
+        {
+            Console.WriteLine("Please Type Your plusSigned Words and MinusSigned and unSigned words");
+            var input = Console.ReadLine()?.ToLower();
+            return input;
+        }
+    }
+}
