ea25c17c7382d5a27d03d1601b92839e88359fe1
amirarab888 <arabzadehamirhossein888@gmail.com>
2021-03-15 11:52:58 +0330

    Implement Partitioner.cs

---
+++ Phase10/Phase10/Phase10Library/Partitioner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Phase10Library
+{
+    public interface IPartitioner
+    {
+        List<string> GetSignedWords(string searchingTerm, string sign);
+        List<string> GetUnSignedWords(string searchingTerm);
+    }
+
+    public class Partitioner : IPartitioner
+    {
+        public List<string> GetSignedWords(string searchingTerm, string sign)
+        {
+            ValidateInputStrings(searchingTerm, sign);
+            var searchingTerms = (Regex.Split(searchingTerm, "\\s")).ToList();
+            return new List<string>(from term in searchingTerms
+                where term.StartsWith(sign)
+                select term.Substring(1).ToLower());
+        }
+
+        public List<string> GetUnSignedWords(string searchingTerm)
+        {
+            ValidateInputString(searchingTerm);
+            var searchingTerms = (Regex.Split(searchingTerm, "\\s")).ToList();
+            return new List<string>(from term in searchingTerms
+                where !term.StartsWith("+") && !term.StartsWith("-")
+                select term.ToLower());
+        }
+
+        private void ValidateInputString(string searchingTerm)
+        {
+            if (IsStringNullOrEmpty(searchingTerm))
+            {
+                throw new ArgumentException("parameter is null or white space");
+            }
+        }
+
+        private bool IsStringNullOrEmpty(string searchingTerm)
+        {
+            return searchingTerm is null || searchingTerm.Trim().Length == 0;
+        }
+
+        private void ValidateInputStrings(string searchingTerm, string sign)
+        {
+            ValidateInputString(searchingTerm);
+            ValidateInputString(sign);
+        }
+    }
+}
---
+++ Phase10/Phase10/Phase10LibraryTest/PartitionerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using Phase10Library;
+using Xunit;
+
+namespace Phase10LibraryTest
+{
+    public class PartitionerTest
+    {
+        private readonly IPartitioner _partitioner;
+        public PartitionerTest()
+        { 
+            _partitioner = new Partitioner();
+        }
+
+        [Fact]
+        public void GetWantedSignedWords_ShouldReturnSignedWords_WhenParametersAreValid()
+        {
+            
+            const string sampleSearchingTerm = "ali reza +mohammad +ghasem -fatemeh -zahra";
+            var expectedReturningList = new List<string>
+            {
+                "mohammad",
+                "ghasem",
+                
+            };
+            Assert.Equal(expectedReturningList, _partitioner.GetSignedWords(sampleSearchingTerm, "+"));
+
+        }
+        public static IEnumerable<object[]> GetWantedSignedWordsInvalidArguments = new List<object[]>
+        {
+            new object[] {It.IsAny<string>(), null},
+            new object[] {It.IsAny<string>(), "   " },
+            new object[] {It.IsAny<string>(), " " },
+            new object[] {" " , It.IsAny<string>()},
+            new object[] {"   " , It.IsAny<string>()},
+        };
+        [Theory]
+        [MemberData(nameof(GetWantedSignedWordsInvalidArguments))]
+        public void GetWantedSignedWords_ShouldThrowArgumentException_WhenParametersAreInvalid(string searchingTerm,
+            string sign)
+        {
+            Action action = () =>
+                _partitioner.GetSignedWords(searchingTerm, sign);
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Fact]
+        public void GetUnSignedWords_ShouldReturnUnsignedWords_WhenParametersAreValid()
+        {
+            const string sampleSearchingTerm = "ali reza +mohammad +ghasem -fatemeh -zahra";
+            var expectedReturningList = new List<string>
+            {
+                "ali",
+                "reza",
+                
+            };
+            Assert.Equal(expectedReturningList, _partitioner.GetUnSignedWords(sampleSearchingTerm));
+
+        }
+        public static IEnumerable<object[]> GetUnsignedWordsInvalidArguments = new List<object[]>
+        {
+            new object[] {null},
+            new object[] {" "},
+            new object[] {"   "},
+        };
+        [Theory]
+        [MemberData(nameof(GetUnsignedWordsInvalidArguments))]
+        public void GetUnsignedWords_ShouldThrowArgumentException_WhenParametersAreInvalid(string searchingTerm)
+        {
+            Action action = () =>
+                _partitioner.GetUnSignedWords(searchingTerm);
+            Assert.Throws<ArgumentException>(action);
+        }
+    }
+}
