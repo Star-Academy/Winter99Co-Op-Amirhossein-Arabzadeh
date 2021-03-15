c07f09bea7104bf826e629ff41f5d98fa0c4acc6
amirarab888 <arabzadehamirhossein888@gmail.com>
2021-03-15 15:27:27 +0330

    Implement TokenFilters.cs enum

--- Phase10/Phase10/Phase10Library/TokenFilters.cs
+++ Phase10/Phase10/Phase10Library/TokenFilters.cs
@@ -3,5 +3,9 @@
     public static class TokenFilters
     {
         public const string NgramFilter = "my-ngram-filter";
+        public static string EnglishStopWords = "stop";
+        public static string WordDelimiter = "word_delimiter";
+        public static string LowerCase = "lowercase";
+        public static string Standard = "standard";
     }
 }
